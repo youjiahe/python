cb71e169349474a8acde88ccda34de6d41bb0632
youjiahe <youjiahe@163.com>
2018-11-10 12:00:30 +0800

    update_2018-11-10_12:00

--- python3/randpass.py
+++ python3/randpass.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 #string某块的ascii_letters函数:所有的大小写字母
 #string某块的printable函数:所有的字符
-from string import ascii_letters
+#string某块的digits函数:所有的数字
+from string import ascii_letters,digits
 from string import printable
 from random import choice
 import sys
+
+listp=ascii_letters+digits
 def password(num=8):
-    randp = ''
+    passwd=''
     for i in range(num):
-        pass1=choice(printable)
-        if pass1 in ['\n',' ','\r','\x0b','\x0c','\t']:
-            continue
-        randp = randp+pass1
-
-    return randp
-
-lo=int(sys.argv[1])
-for i in range(10):
-    print(password(lo))
+        p0=choice(listp)
+        passwd += p0
+    return passwd
+if sys.argv.__len__() ==2:
+    for i in sys.argv[1]:
+        if i not in digits:
+            print('请输入一个数字')
+            exit()
+    else:
+        lo = int(sys.argv[1])
+        print(password(lo))
+else:
+    print(password())
