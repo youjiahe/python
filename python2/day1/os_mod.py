4cb0916d7f4de3d0bab29f5299583363fc9438b7
youjiahe <youjiahe@163.com>
2018-11-13 18:57:21 +0800

    update_2018-11-13_18:57

--- python2/day1/os_mod.py
+++ python2/day1/os_mod.py
@@ -8,3 +8,6 @@ import shutil
 shutil.copy2('/etc/passwd','/opt/test/')
 os.remove('/opt/test/passwd')
 print(os.getcwd())
+os.chdir('/root')
+print(os.getcwd())
+os.chdir('/root/git/python/python2/day1')
