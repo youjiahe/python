c2466eabf3ac6818dfbbbbc965d8dd819690bf57
youjiahe <youjiahe@163.com>
2018-11-14 18:51:37 +0800

    update_2018-11-14_18:51

---
+++ python2/day2/set_coloar.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def set_color(func):
+    def color():
+        return "\033[31;1m%s\033[0m" % func()
+    return color
+
+def hello():
+    return "Hello World!"
+
+@set_color
+def greet():
+    return "How are you?"
+
+if __name__ == '__main__':
+    set_color(hello)
