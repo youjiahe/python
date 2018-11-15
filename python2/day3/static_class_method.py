06b96eabea7aa6f7e4cb9bda6bfcbb8806a307bf
youjiahe <youjiahe@163.com>
2018-11-15 18:42:29 +0800

    update_2018-11-15_18:42

--- python2/day3/static_class_method.py
+++ python2/day3/static_class_method.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 class SC:
-    def hello():
+    def hello(self):
         print('Hello World')
+
     @staticmethod
     def welcome():
         print('welcome back Rose!')
 
+
+
 if __name__ == '__main__':
-    SC.hello()  #需要实例，没有的话则报错
-    SC.welcome()
+    SC.hello(15)  #需要实例，没有的话则报错
+    SC.welcome()  #静态类，不需要
