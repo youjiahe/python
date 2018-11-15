766a203f2a20b0c016363c6e5cb46b02d6708ccb
youjiahe <youjiahe@163.com>
2018-11-15 12:58:20 +0800

    update_2018-11-15_12:58

--- python2/day3/del_repl.py
+++ python2/day3/del_repl.py
@@ -3,7 +3,7 @@ def del_re(list=[]):
     while list:
         for i in list:
             ind=list.index(i)
-            for j in list[ind:]:
+            for j in list[ind+1:-1]:
                 if j==i:
                     list.pop(list.index(j))
 li=list('hello')
