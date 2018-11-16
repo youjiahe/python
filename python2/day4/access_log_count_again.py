a94c89bca5b9bc85392b643e43bb446c2d457d24
youjiahe <youjiahe@163.com>
2018-11-16 18:53:22 +0800

    update_2018-11-16_18:53

--- python2/day4/access_log_count_again.py
+++ python2/day4/access_log_count_again.py
@@ -7,11 +7,22 @@ class CountAccess:
         self.patt=patt
 
     def __call__(self):
-        pass
+        c=Counter()
+        cpatt=compile(self.patt)
+        with open(fname) as fobj:
+            for line in fobj:
+                m = cpatt.search(self.patt, line)
+                if m:
+                    c.update(m.group())
+                
+    def most(self,num):
+
 
 if __name__ == '__main__':
     fname='access.log'
     ip='(\d{1,3}\.){3}\d{3}'
     br='Chrome|Firefox|MISE'
     print(CountAccess(fname,ip))
-    print()
+    print(CountAccess(fname,br))
+    print(CountAccess(fname, ip).most(2))
+    print(CountAccess(fname, br).most(2))
