4078dd694bb569bb17b31b724129aceec3e867e2
youjiahe <youjiahe@163.com>
2018-11-26 20:30:33 +0800

    update_2018-11-26_20:30

--- devops/day6/myansible/mymodule/mycopy_.py
+++ devops/day6/myansible/mymodule/mycopy_.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-# coding: utf8
-import  shutil
+import shutil
 from ansible.module_utils.basic import AnsibleModule
 
 def main():
-    '函数名为 main，必须的'
     module = AnsibleModule(
         argument_spec=dict(
             src=dict(required=True,type='str'),
-            dst=dict(required=True,type='str')
+            dst=dict(required=True, type='str')
         )
     )
     shutil.copy(module.params['src'],module.params['dst'])
