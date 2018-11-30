e5847b3f0181a490dcd98f720fe5edfa23f93087
youjiahe <youjiahe@163.com>
2018-11-30 20:30:37 +0800

    update_2018-11-30_20:30

--- devweb/day4/mysite/poll/admin.py
+++ devweb/day4/mysite/poll/admin.py
@@ -3,6 +3,10 @@ from .models import Questions,Choice
 # Register your models here.
 #admin.site.register(Questions)
 class QuestionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['question_text','pub_date']
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    date_hierarchy = 'pub_date'
+    ordering = ['-pub_date']
 admin.site.register(Questions,QuestionAdmin)
 admin.site.register(Choice)
