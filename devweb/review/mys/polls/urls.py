8267c456797b2f75a443ffdceb47e22dd6fd9351
youjiahe <youjiahe@163.com>
2018-12-02 21:36:19 +0800

    update devweb

---
+++ devweb/review/mys/polls/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+urlpatterns=[
+    url(r'^$',views.index,name='index'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/resutl/$', views.result, name='result'),
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+]
